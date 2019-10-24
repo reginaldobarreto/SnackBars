f1b1ef5bf755aa5a99584d520439e0f5c4d53d1d
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-24 10:10:39 -0300

    adicionado ação ao floatingActionButton e uma SnackBar

--- app/src/main/java/br/com/barrsoft/snackbars/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/snackbars/MainActivity.java
@@ -3,12 +3,41 @@ package br.com.barrsoft.snackbars;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getName();
+    @BindView(R.id.floatingActionButton)
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ButterKnife.bind(this);
+
+
     }
+
+    @OnClick(R.id.floatingActionButton)
+    public void submit(View view) {
+        // TODO submit data to server...
+
+        Log.i(TAG, "submit:");
+        Snackbar snackbar = Snackbar.make(view,"Teste",Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
 }
